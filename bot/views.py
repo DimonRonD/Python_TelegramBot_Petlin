2c6abb177f07315315aee1c3d6d42d1f384b625f
Dmitry Petlin <DimonRonD@gmail.com>
2025-08-21 17:11:28 +0300

    FEATURE: added auth page, templates, first view

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
+
+
+def auth_site(request):
+    template = loader.get_template('auth.html')
+    context = {}
+    rendered_page = template.render(context, request)
+    return HttpResponse(rendered_page)
--- core/urls.py
+++ core/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from bot.views import auth_site
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", auth_site)
 ]
